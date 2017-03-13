fad7bbe2f6ed101a3f3bfe6dedfc8a74db475f67
adam <wjw3323@live.com>
2017-03-13 21:08:38 +0800

    add app path

--- frame/bootstrap.php
+++ frame/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 use LittlePeach\Kernel;
 
+define('FRAME_PATH', dirname(__DIR__));
+define('APP_PATH', FRAME_PATH.'../app');
 $autoload = require_once(__DIR__."/vendor/autoload.php");
-$autoload->addPsr4("", "");
+$autoload->addPsr4("\\", "src");
 
 $kernel = new Kernel();
 $kernel->run();
--- webroot/index.php
+++ webroot/index.php
@@ -5,7 +5,6 @@
  * Date: 2017-03-13
  * Time: 下午 2:35
  */
-define('ROOT_PATH', '../');
-define('FRAME_PATH', '../frame');
+
 
 require_once("../frame/bootstrap.php");
