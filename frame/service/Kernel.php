19d5f91ed7791a4ec98fabee8ff09699e01b8879
adam123 <wjw3323@live.com>
2017-07-03 18:02:06 +0800

    move init to common

--- frame/service/Kernel.php
+++ frame/service/Kernel.php
@@ -7,12 +7,15 @@
  */
 
 namespace LittlePeach\Service;
+
+use LittlePeach\library\Log;
 use LittlePeach\Service\Middleware\Middleware;
 use LittlePeach\Service\Middleware\RequestHandleMiddleware;
-use LittlePeach\Utility\Common;
 use LittlePeach\Utility\Delegate;
 use Restore\Container;
 use Symfony\Component\HttpFoundation\Request;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 class Kernel
 {
@@ -32,16 +35,68 @@ class Kernel
      * @var Request
      */
     private $request;
+    /**
+     * @var Kernel
+     */
+    protected static $kernel;
     public function __construct($debug = 0)
     {
         $this->debug = $debug;
-        $this->delegate = Common::createDelegate();
-        $this->container = Common::createContainer();
-        $this->request = Common::initRequest();
+        $this->init();
+        self::$kernel = $this;
+    }
+    private function createContainer()
+    {
+        return new Container();
+    }
+
+    private function createDelegate()
+    {
+        return new Delegate();
+    }
+
+    private function registerWhoopsHandle()
+    {
+        $whoops = new Run();
+        $whoops->pushHandler(new PrettyPageHandler);
+        $whoops->register();
+    }
+
+    private function registerErrorHandle()
+    {
+        set_error_handler(function ($errno, $errstr, $errfile, $errline = '', $errcontext = '') {
+            Log::getInstance()->onError($errno, $errstr, $errfile, $errline, $errcontext);
+        });
+        set_exception_handler(function (\Throwable $exception) {
+            Log::getInstance()->onException($exception);
+        });
+        register_shutdown_function(function () {
+            $error = error_get_last();
+            Log::getInstance()->onShutDown($error);
+        });
+    }
+
+    /**
+     * @return Request
+     */
+    private function initRequest()
+    {
+        $request = Request::createFromGlobals();
+        unset($_REQUEST, $_SERVER, $_GET, $_POST, $_FILES);
+        return $request;
+    }
+
+    private function init()
+    {
+        $this->delegate = $this->createDelegate();
+        $this->container = $this->createContainer();
+        $this->request = $this->initRequest();
+        $this->registerErrorHandle();
         if ($this->getDebugLevel(self::DISPLAY_ERROR)){
-            Common::registerErrorHandle();
+            $this->registerWhoopsHandle();
         }
     }
+
     public function run()
     {
         $this->dispatch(new RequestHandleMiddleware());
@@ -64,5 +119,10 @@ class Kernel
     public function dispatch(Middleware $middleware)
     {
         $this->delegate->enqueue($middleware);
+    }
+
+    public function getInstance()
+    {
+        return static::$kernel;
     }
 }
